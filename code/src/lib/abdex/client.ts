91d0c5646d22aeb467721057ef851692cb79bae5
zeng zhenhua <160127vip@gmail.com>
2023-08-27 10:47:58 +0800

    add

--- code/src/lib/abdex/client.ts
+++
@@ -1,7 +0,0 @@
-import { Client } from '@lib/ts-client';
-
-export const abdexClient = new Client({
-	apiURL: 'http://localhost:1317',
-	rpcURL: 'http://localhost:26657',
-	prefix: 'abdex'
-});
--- code/src/lib/utils/rest.ts
+++
@@ -1,4 +0,0 @@
-// 编码rest的请求参数 / -> __, : -> ..
-export const EncodeRestStr = (str: string) => {
-	return str.replace(/\//g, '__').replace(/:/g, '..');
-};
